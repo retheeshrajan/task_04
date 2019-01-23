8b0def477a8ff701858724ee684d1b9828e90a06
unknown <retheesh@TENT-Retheesh.tamdeenrealestate.com>
2019-01-23 18:27:44 +0300

    Retheesh & Rashed changed TimeField

--- restaurants/models.py
+++ restaurants/models.py
@@ -6,8 +6,8 @@ class Restaurant(models.Model):
 
 	name=models.CharField(max_length=30)
 	description=models.TextField()
-	opening_time=models.CharField(max_length=30)
-	closing_time=models.CharField(max_length=30)
+	opening_time=models.TimeField()
+	closing_time=models.TimeField()
 
 	def __str__(self):
 		return self.name
