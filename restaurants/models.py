d05b19fc0138bde6d3bd411ac53b8a6a4824bac1
unknown <retheesh@TENT-Retheesh.tamdeenrealestate.com>
2019-01-23 18:20:59 +0300

    Retheesh & Rashed

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	"""docstring for ClassName"""
+
+	name=models.CharField(max_length=30)
+	description=models.TextField()
+	opening_time=models.CharField(max_length=30)
+	closing_time=models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.name
+
